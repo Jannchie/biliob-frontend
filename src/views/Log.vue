d2d91ffe82de32594e9e215cf612578cab43d8db
Jannchie <jannchie@gmail.com>
2018-11-20 14:01:06 +0800

    reconstitution faq page

--- src/views/Log.vue
+++ src/views/Log.vue
@@ -1,59 +1,75 @@
 <template>
   <v-timeline dense>
-    <v-timeline-item :key="n" color="red lighten-2" large>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">项目启动</v-card-title>
-        <v-card-title class="title">2018-08-03</v-card-title>
-        <v-card-text>
-            初始化了前后端工程，标志着该项目正式启动。
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-        <v-timeline-item :key="n" color="red lighten-2" large>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">实现UP主以及视频数据的浏览</v-card-title>
-        <v-card-title class="title">2018-10-18</v-card-title>
-        <v-card-text>
-            实现了该网站最最基本的两大功能，现在这已经是一个可以勉强一用的网站了。
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-     <v-timeline-item :key="n" color="red lighten-2" large>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">基本数据可视化</v-card-title>
-        <v-card-title class="title">2018-10-23</v-card-title>
-        <v-card-text>
-            加入了最基本的几张图表的可视化。
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-         <v-timeline-item :key="n" color="red lighten-2" large>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">使用Vuetify</v-card-title>
-        <v-card-title class="title">2018-10-26</v-card-title>
-        <v-card-text>
-            使用Vuetify完全地重构了前端的代码。现在能够更加合理地在手机端和电脑端无差别使用。
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-             <v-timeline-item :key="n" color="red lighten-2" large>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">完成注册功能</v-card-title>
-        <v-card-text class="title">2018-10-28</v-card-text>
-        <v-card-text>
-            现在已经能够注册并进行基本的操作了！
-        </v-card-text>
-      </v-card>
-    </v-timeline-item>
-
-                 <v-timeline-item :key="n" color="red lighten-2" large>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">添加favorite</v-card-title>
-        <v-card-text class="title">2018-11-10</v-card-text>
-        <v-card-text>
-            现在能够添加喜欢的UP主和视频了！
-        </v-card-text>
-      </v-card>
+     <v-timeline-item :key="eachPost.title" v-for="eachPost in timelinePost" fill-dot :color="getColor(eachPost.type)" :icon="getIcon(eachPost.type)" :small="isSmall(eachPost.type)">
+          <v-layout pt-3>
+            <v-flex>
+              <strong>{{eachPost.title}}</strong>
+              <strong class="caption mb-1">{{eachPost.date}}</strong>
+              <div class="caption mb-2">{{eachPost.text}}</div>
+            </v-flex>
+          </v-layout>
     </v-timeline-item>
   </v-timeline>
 </template>
+<script>
+export default {
+  methods: {
+    isSmall: function (type) {
+      let isSmall
+      type === 'milestone' ? isSmall = false : isSmall = true
+      return isSmall
+    },
+    getIcon: function (type) {
+      switch (type) {
+        case 'feature':
+          return 'mdi-lightbulb'
+        case 'structure':
+          return 'mdi-cloud'
+        case 'milestone':
+          return 'mdi-flag'
+        default:
+          break
+      }
+    },
+    getColor: function (type) {
+      switch (type) {
+        case 'feature':
+          return 'orange'
+        case 'structure':
+          return 'blue'
+        case 'milestone':
+          return 'green'
+        default:
+          break
+      }
+    }
+  },
+  data () {
+    return {
+      timelinePost: [
+        {
+          title: '项目启动',
+          date: '2018-08-03',
+          text: '初始化了前后端工程，标志着该项目正式启动。',
+          type: 'milestone'
+        }, {
+          title: '功能更新 ver.0.0.1',
+          date: '2018-10-26',
+          text: '完成了基本的数据可视化，完成了注册登录模块，使用Vuetify完全地重构了前端的代码。现在能够更加合理地在手机端和电脑端无差别使用。',
+          type: 'feature'
+        }, {
+          title: '首次公开',
+          date: '2018-10-28',
+          text: '第一次公开这个网站，收获了首批用户。',
+          type: 'milestone'
+        }, {
+          title: '架构更新',
+          date: '2018-11-10',
+          text: '正式启用域名，使用CDN缓存，使用Ngnix反向代理。',
+          type: 'structure'
+        }
+      ]
+    }
+  }
+}
+</script>
