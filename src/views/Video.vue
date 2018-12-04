cc562ee22c4f8486a399f6f2a72440c01fdc00eb
Jannchie <jannchie@gmail.com>
2018-12-04 21:14:12 +0800

    fix: the abnormal behavior of the focus button

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -2,7 +2,7 @@
   <MainLayout>
     <VideoMain slot="main-cards"></VideoMain>
     <VideoAside slot="aside-cards">
-      <AuthorInfo slot="author-info"></AuthorInfo>
+      <AuthorOperation slot="author-info"></AuthorOperation>
       <OtherVideo slot="other-video"></OtherVideo>
       <Recommand slot="recommand"></Recommand>
     </VideoAside>
@@ -13,7 +13,7 @@
 import MainLayout from "../components/common/MainLayout.vue";
 import VideoAside from "../components/aside/VideoAside.vue";
 import VideoMain from "../components/main/VideoMain.vue";
-import AuthorInfo from "../components/aside/AuthorInfo.vue";
+import AuthorOperation from "../components/aside/AuthorOperation.vue";
 import OtherVideo from "../components/aside/OtherVideo.vue";
 import Recommand from "../components/aside/Recommand.vue";
 export default {
@@ -22,7 +22,7 @@ export default {
     MainLayout,
     VideoAside,
     VideoMain,
-    AuthorInfo,
+    AuthorOperation,
     OtherVideo,
     Recommand
   },
