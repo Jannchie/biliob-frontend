01ad592ce9bc34f9ddda12e08d0a13c58b1afac3
Jannchie <jannchie@gmail.com>
2018-12-08 16:50:30 +0800

    feature: beautify focus button

--- src/components/common/FocusBtn.vue
+++ src/components/common/FocusBtn.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
-      <VBtn v-if="!focus" :disabled="disabled" depressed color="primary" @click.stop="addFocus"><VIcon>mdi-plus</VIcon><span>&nbsp;未关注</span></VBtn>
-      <VBtn v-if="focus" :disabled="disabled" depressed @click.stop="removeFocus"><VIcon>mdi-star</VIcon>&nbsp;已关注</VBtn>
-    <VTooltip v-model="show" bottom>
-      <div slot="activator"></div>
-      <span>{{text}}</span>
-    </VTooltip>
-
+      <VBtn v-if="!focus" color="grey lighten-1" flat icon depressed @click.stop="addFocus"><VIcon>mdi-heart</VIcon><span></span></VBtn>
+      <VBtn v-if="focus" color="red lighten-1" flat icon depressed @click.stop="removeFocus"><VIcon>mdi-heart</VIcon></VBtn>
   </div>
 </template>
 <script>
@@ -15,9 +10,7 @@ export default {
   data() {
     return {
       focus: Boolean(),
-      text: String(),
-      show: false,
-      disabled: false
+      show: false
     };
   },
   watch: {
@@ -34,46 +27,26 @@ export default {
     addFocus() {
       this.axios
         .post(`/user/author`, Number(this.authorData.mid))
-        .then(response => {
-          this.disabled = true;
+        .then(() => {
           this.show = true;
-          this.text = response.data.msg;
           this.$store.commit("addFavoriteAuthor", this.authorData.mid);
-          setTimeout(() => {
-            this.disabled = false;
-            this.show = false;
-          }, 2000);
           this.focus = !this.focus;
         })
-        .catch(error => {
-          this.disabled = true;
+        .catch(() => {
           this.show = true;
-          this.text = error.response.data.msg;
-          setTimeout(() => {
-            this.disabled = false;
-            this.show = false;
-          }, 2000);
           this.focus = !this.focus;
         });
     },
     removeFocus() {
       this.axios
         .delete(`/user/author/${this.authorData.mid}`)
-        .then(response => {
+        .then(() => {
           this.show = true;
-          this.text = response.data.msg;
           this.$store.commit("removeFavoriteAuthor", this.authorData.mid);
-          setTimeout(() => {
-            this.show = false;
-          }, 2000);
           this.focus = !this.focus;
         })
-        .catch(error => {
+        .catch(() => {
           this.show = true;
-          this.text = error.response.data.msg;
-          setTimeout(() => {
-            this.show = false;
-          }, 2000);
           this.focus = !this.focus;
         });
     }
