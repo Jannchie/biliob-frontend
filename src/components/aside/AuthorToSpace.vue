bed972bd82e422ac58bd6172fc80ec618bf52aca
Jannchie <jannchie@gmail.com>
2018-12-09 15:51:18 +0800

    feature: jump to author's space at author's detail page.

---
+++ src/components/aside/AuthorToSpace.vue
@@ -0,0 +1,33 @@
+<template>
+<VCard>
+    <VCardText>
+        <VBtn block color="#ff7fac" :href="url"  target="_blank">前往UP主的个人空间</VBtn>
+    </VCardText>
+</VCard>
+</template>
+<script>
+export default {
+  props: {
+    mid: Number()
+  },
+  computed: {
+    url: function() {
+      return `https://space.bilibili.com/${this.mid}`;
+    }
+  },
+  watch: {
+    mid: function(val) {
+      this.mid = val;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.v-card {
+  margin-bottom: 5px;
+}
+.v-btn {
+  color: #ffffff;
+}
+</style>
