8f0c6ce961c1624971028ef68ef6ff6d237e432f
Jannchie <jannchie@gmail.com>
2019-02-13 22:01:58 +0800

    feature: author rank

---
+++ src/components/common/ChevronBudget.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="caption font-weight-bold grey--text text--darken-2">
+    <VIcon
+      :class="color"
+      small
+    >{{icon}}</VIcon><span :class="color">{{value}}</span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: Number()
+  },
+  computed: {
+    icon() {
+      if (this.value > 0) {
+        return "mdi-chevron-up";
+      } else if (this.value < 0) {
+        return "mdi-chevron-down";
+      } else {
+        return "mdi-minus";
+      }
+    },
+    color() {
+      if (this.value > 0) {
+        return "red--text";
+      } else if (this.value < 0) {
+        return "green--text";
+      } else {
+        return "grey--text";
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/main/AuthorDetailRank.vue
@@ -0,0 +1,87 @@
+<template>
+  <VCard>
+    <VCardText style="height:100%">
+      <div class="rank-container">
+        <div style="flex-grow: 1;text-align:center">
+          <div class="rank-title subheading font-weight-bold grey--text text--darken-2">
+            粉丝数排名
+          </div>
+          <div class="display-1 font-weight-black blue--text text--darken-3">
+            {{beautify(fansRank)}}
+          </div>
+          <ChevronBudget :value="dFansRank"></ChevronBudget>
+        </div>
+        <VDivider vertical></VDivider>
+        <div style="flex-grow: 1;text-align:center">
+          <div class="rank-title subheading font-weight-bold grey--text text--darken-2">
+            播放量排名
+          </div>
+          <div class="display-1 font-weight-black blue--text text--darken-3">
+            {{beautify(archiveViewRank)}}
+          </div>
+          <ChevronBudget :value="dArchiveViewRank"></ChevronBudget>
+        </div>
+        <VDivider vertical></VDivider>
+        <div style="flex-grow: 1;text-align:center">
+          <div class="rank-title subheading font-weight-bold grey--text text--darken-2">
+            专栏阅读排名
+          </div>
+          <div class="display-1 font-weight-black blue--text text--darken-3">
+            {{beautify(articleViewRank)}}
+          </div>
+          <ChevronBudget :value="dArticleViewRank"></ChevronBudget>
+        </div>
+      </div>
+    </VCardText>
+    <VCardText
+      class="caption grey--text"
+      style="display:flex;justify-content:space-between"
+    >
+      <div>
+        此排行仅包括已经观测UP主，仅供参考。
+      </div>
+      <div>
+        最后更新日期时间：{{format(updateTime)}}
+      </div>
+    </VCardText>
+  </VCard>
+</template>
+
+<script>
+var format = require("date-fns/format");
+var { convertDateToUTC } = require("../../charts/util/convertDateToUTC");
+import ChevronBudget from "../common/ChevronBudget.vue";
+export default {
+  components: { ChevronBudget },
+  props: {
+    fansRank: Number(),
+    articleViewRank: Number(),
+    archiveViewRank: Number(),
+    dFansRank: Number(),
+    dArticleViewRank: Number(),
+    dArchiveViewRank: Number(),
+    updateTime: String()
+  },
+  methods: {
+    format(date) {
+      return format(convertDateToUTC(new Date(date)), "YYYY-MM-DD HH:MM:SS");
+    },
+    beautify(val) {
+      if (val === -1) {
+        return "-";
+      } else {
+        return val;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.rank-container {
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+  height: 100%;
+}
+</style>
