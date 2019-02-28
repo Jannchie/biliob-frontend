268a703e002bfe0e3cd52ca001788bfc70d17988
Jannchie <jannchie@gmail.com>
2019-03-01 00:04:32 +0800

    feature: author top precent rank

--- src/components/main/AuthorDetailRank.vue
+++ src/components/main/AuthorDetailRank.vue
@@ -6,8 +6,8 @@
           <div class="rank-title subheading font-weight-bold grey--text text--darken-2">
             粉丝数排名
           </div>
-          <div class="display-1 font-weight-black blue--text text--darken-3">
-            {{beautify(fansRank)}}
+          <div class="title font-weight-black blue--text text--darken-3">
+            {{fansInfo}}
           </div>
           <ChevronBudget :value="dFansRank"></ChevronBudget>
         </div>
@@ -18,8 +18,8 @@
           <div class="rank-title subheading font-weight-bold grey--text text--darken-2">
             播放量排名
           </div>
-          <div class="display-1 font-weight-black blue--text text--darken-3">
-            {{beautify(archiveViewRank)}}
+          <div class="title font-weight-black blue--text text--darken-3">
+            {{archiveInfo}}
           </div>
           <ChevronBudget :value="dArchiveViewRank"></ChevronBudget>
         </div>
@@ -30,8 +30,8 @@
           <div class="rank-title subheading font-weight-bold grey--text text--darken-2">
             专栏阅读排名
           </div>
-          <div class="display-1 font-weight-black blue--text text--darken-3">
-            {{beautify(articleViewRank)}}
+          <div class="title font-weight-black blue--text text--darken-3">
+            {{articleInfo}}
           </div>
           <ChevronBudget :value="dArticleViewRank"></ChevronBudget>
         </div>
@@ -64,6 +64,9 @@ export default {
     dFansRank: Number(),
     dArticleViewRank: Number(),
     dArchiveViewRank: Number(),
+    pFansRank: Number(),
+    pArticleViewRank: Number(),
+    pArchiveViewRank: Number(),
     updateTime: String()
   },
   computed: {
@@ -76,14 +79,45 @@ export default {
       } else {
         return "载入更新时间中";
       }
+    },
+    fansInfo() {
+      if (this.fansRank == undefined) {
+        return "";
+      }
+      if (this.fansRank <= 200 && this.fansRank != -1) {
+        return `Top ${this.fansRank}`;
+      } else {
+        return this.beautify(this.pFansRank);
+      }
+    },
+    articleInfo() {
+      if (this.articleViewRank == undefined) {
+        return "";
+      }
+      if (this.articleViewRank <= 200 && this.articleViewRank != -1) {
+        return `Top ${this.pArticleViewRank}`;
+      } else {
+        return this.beautify(this.pArticleViewRank);
+      }
+    },
+    archiveInfo() {
+      if (this.archiveViewRank == undefined) {
+        return "";
+      }
+      if (this.archiveViewRank <= 200 && this.archiveViewRank != -1) {
+        return `Top ${this.archiveViewRank}`;
+      } else {
+        return this.beautify(this.pArchiveViewRank);
+      }
     }
   },
+
   methods: {
     beautify(val) {
       if (val === -1) {
         return "-";
       } else {
-        return val;
+        return `前${val}%`;
       }
     }
   }
