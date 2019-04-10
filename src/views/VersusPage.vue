2a161520bfb886923d5160e5b3578e9590664706
Jannchie <jannchie@gmail.com>
2019-04-10 21:30:12 +0800

    feature: FAQ index

---
+++ src/views/VersusPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <DetailCharts />
+  </div>
+</template>
+<script>
+import DetailCharts from "../components/main/DetailCharts.vue";
+// import getOnlineOptions from "../charts/online.js";
+
+export default {
+  components: { DetailCharts },
+  data() {
+    return {
+      siteData: Object(),
+      onlineOptions: Object(),
+      firstMid: Number(),
+      secondMid: Number()
+    };
+  },
+  mounted() {
+    this.$store.commit("toElse");
+    this.axios.get("/author?page=0&pageSize=2&sort=0").then(r => {
+      this.firstMid = r.data.content[0].mid;
+      this.secondMid = r.data.content[1].mid;
+    });
+  }
+};
+</script>
