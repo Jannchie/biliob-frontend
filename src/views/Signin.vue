3fa8235258e8d41f6b2e671a88a227f40151200d
Jannchie <jannchie@gmail.com>
2019-05-28 19:43:16 +0800

    enhance: log in and sign in

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,34 +1,29 @@
 <template>
-  <VLayout
-    v-bind="binding"
-    justify-center
-  >
+  <VLayout justify-center>
     <VFlex
       lg5
       md12
     >
-      <VCard>
-        <div>
-          <VAlert
+      <MaterialCard title="注册">
+        <div style="position: absolute;left:15px;right:15px;">
+          <MaterialNotification
             :value="alertSuccess"
             type="success"
             transition="scale-transition"
-          >恭喜你！注册成功！</VAlert>
-          <VAlert
+          >恭喜！操作成功！</MaterialNotification>
+          <MaterialNotification
             :value="alertError"
             type="error"
             transition="scale-transition"
-          >抱歉！{{ errorMsg }}!</VAlert>
+          >抱歉！{{ errorMsg }}!</MaterialNotification>
         </div>
 
-        <VCardTitle>
-          <div>
-            <h3 class="headline mb-1">注册</h3>
-            <div>这是一个第三方网站，账号信息和B站并不通用。
-              <br>登录后可以自主添加视频追踪，还能够实现关注UP主和收藏视频的功能（现在也许正在施工中、逃。
-            </div>
+        <div>
+          <div class="body-1">这是一个第三方网站，账号信息和B站并不通用。
+            <br>登录后可以自主添加视频追踪，还能够实现关注UP主和收藏视频的功能。
+            <br>登录后还能签到获取积分，消耗积分获取更多数据情报。
           </div>
-        </VCardTitle>
+        </div>
         <VCardActions>
           <VForm
             v-model="valid"
@@ -40,6 +35,33 @@
               browser-autocomplete="username"
               label="请输入用户名"
             />
+            <VTextField
+              v-model="mail"
+              :readonly="mailDisabled"
+              :rules="[rules.required,rules.email]"
+              browser-autocomplete="mail"
+              label="请输入邮箱"
+            />
+            <VLayout>
+              <VFlex md8>
+                <VTextField
+                  v-model="activationCode"
+                  :rules="[rules.required]"
+                  label="请输入验证激活码"
+                />
+              </VFlex>
+              <VFlex
+                md4
+                class="align-self-center"
+              >
+                <VBtn
+                  flat
+                  color="primary"
+                  :disabled="!mailVerification(mail)"
+                  @click.stop="sendActivationCode"
+                >向邮箱发送验证码</VBtn>
+              </VFlex>
+            </VLayout>
             <VTextField
               v-model="password"
               :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
@@ -71,7 +93,7 @@
             </Center>
           </VForm>
         </VCardActions>
-      </VCard>
+      </MaterialCard>
     </VFlex>
   </VLayout>
 </template>
@@ -81,16 +103,22 @@ export default {
     return {
       name: "",
       password: "",
+      mail: "",
       valid: false,
       show: null,
       alertSuccess: false,
       alertError: false,
       errorMsg: "",
+      mailDisabled: false,
+      activationCode: "",
       rules: {
-        required: value => !!value || "求求你告诉我吧o(╥﹏╥)o",
-        min: v => v.length >= 6 || '这么短小的话，也太不安全了吧Σ(⊙▽⊙"a',
+        required: value => !!value || "我必须知道这一项！求求你告诉我吧~",
+        min: v => v.length >= 6 || "这么短小的话，也太不安全了吧",
         passwdMatch: v => {
-          return v === this.password || "貌似两次输入不一致..手速太快了？罒ω罒";
+          return v === this.password || "貌似两次输入不一致..手速太快了？";
+        },
+        email: v => {
+          return this.mailVerification(v) || "据我所知，邮箱好像不长这样啊！";
         }
       }
     };
@@ -99,11 +127,16 @@ export default {
     this.$store.commit("toElse");
   },
   methods: {
+    mailVerification(v) {
+      return /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/.test(v);
+    },
     submit() {
       this.axios
         .post("/user", {
           name: this.name,
-          password: this.password
+          password: this.password,
+          activationCode: this.activationCode,
+          mail: this.mail
         })
         .then(() => {
           this.alertSuccess = true;
@@ -116,6 +149,28 @@ export default {
         .catch(error => {
           this.alertError = true;
           this.errorMsg = error.response.data.msg;
+          setTimeout(() => {
+            this.alertError = false;
+          }, 2000);
+        });
+    },
+    sendActivationCode() {
+      this.mailDisabled = true;
+      this.axios
+        .post(`/user/activation-code?mail=${this.mail}`)
+        .then(() => {
+          this.alertSuccess = true;
+          this.alertError = false;
+          setTimeout(() => {
+            this.alertSuccess = false;
+          }, 2000);
+        })
+        .catch(error => {
+          this.alertError = true;
+          this.errorMsg = error.response.data.msg;
+          setTimeout(() => {
+            this.alertError = false;
+          }, 2000);
         });
     }
   }
