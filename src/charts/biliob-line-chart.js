450743241a64735b0365a6ed5b0b0b586a7cd387
Jannchie <jannchie@gmail.com>
2019-11-27 00:27:38 +0900

    ENHANCEMENT: Separate author history chart cards.

---
+++ src/charts/biliob-line-chart.js
@@ -0,0 +1,96 @@
+var format = require("date-fns/format");
+var { convertDateToUTC } = require("./util/convertDateToUTC");
+function drawChart(data, name, color = "#1e88e5") {
+  console.log(data);
+
+  let Chart = {
+    legend: {
+      data: [name],
+      bottom: "5px"
+    },
+    grid: {
+      left: "10px",
+      right: "50px",
+      top: "10px",
+      containLabel: true
+    },
+    dataZoom: [
+      {
+        type: "inside",
+        filterMode: "weakFilter"
+      },
+      {
+        handleSize: "100%",
+        handleStyle: {},
+        bottom: "20px"
+      }
+    ],
+    tooltip: {
+      trigger: "axis",
+      confine: true,
+      axisPointer: {
+        label: {
+          formatter: function(params) {
+            return Math.round(params.value);
+          }
+        }
+      }
+    },
+    xAxis: {
+      type: "time",
+      axisPointer: {
+        label: {
+          formatter: function(params) {
+            return (
+              "日期：" +
+              format(
+                convertDateToUTC(new Date(params.value)),
+                "YYYY-MM-DD HH:mm"
+              )
+            );
+          }
+        }
+      }
+    },
+    color: [color],
+    yAxis: [
+      {
+        type: "value",
+        min: "dataMin",
+        axisLabel: {
+          formatter: function toThousands(num) {
+            let postfix = "";
+            if (num > 100000000) {
+              postfix = "亿";
+              num /= 100000000;
+            } else if (num > 10000) {
+              postfix = "万";
+              num /= 10000;
+            } else {
+              return num;
+            }
+            num = num.toFixed(2);
+            let [int, dec] = num.split(".");
+            let inter = "";
+            while (int != "") {
+              inter = int.slice(-3) + "," + inter;
+              int = int.slice(0, -3);
+            }
+            return `${inter.slice(0, -1)}.${dec}${postfix}`;
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: name,
+        data: data,
+        smooth: true,
+        showSymbol: false,
+        type: "line"
+      }
+    ]
+  };
+  return Chart;
+}
+export default drawChart;
