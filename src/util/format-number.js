16ee46eb1abce329e0f35868567867b9c935d5c4
Jannchie <jannchie@gmail.com>
2019-12-30 20:04:27 +0800

    EXTRACT: format number method

---
+++ src/util/format-number.js
@@ -0,0 +1,25 @@
+function formatNumber(num) {
+  let absNum = Math.abs(num);
+  let postfix = "";
+  if (absNum >= 100000000) {
+    postfix = "亿";
+    num /= 100000000;
+  } else if (absNum >= 10000000) {
+    postfix = "千万";
+    num /= 10000000;
+  } else if (absNum >= 10000) {
+    postfix = "万";
+    num /= 10000;
+  } else {
+    return num;
+  }
+  num = num.toFixed(1);
+  let [int, dec] = num.split(".");
+  let inter = "";
+  while (int != "") {
+    inter = int.slice(-3) + "," + inter;
+    int = int.slice(0, -3);
+  }
+  return `${inter.slice(0, -1)}.${dec}${postfix}`;
+}
+export default formatNumber;
