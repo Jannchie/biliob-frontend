558407e9d5adfa00452d353d62438d5114df3e09
Jannchie <jannchie@gmail.com>
2020-05-10 02:07:50 +0900

    UPDATE: Info Dialog

--- src/components/biliob/InfoDialog.vue
+++ src/components/biliob/InfoDialog.vue
@@ -2,18 +2,16 @@
   <VDialog persistent :value="show" width="500">
     <VCard>
       <VCardTitle>
-        <VCard dark color="red" class="info-offset">
-          <VIcon size="24px" class="ma-4">mdi-skull-crossbones</VIcon
-          >服务器出现异常
+        <VCard dark color="warning" class="info-offset">
+          <VIcon size="24px" class="ma-4"> mdi-power </VIcon>服务器临时维护中
         </VCard>
       </VCardTitle>
       <VCardText>
-        如果你看到这段文字，说明可能出现了两种情况：
-        <li>网站炸了</li>
-        <li>网站正在维护中</li>
-        由于这是一个个人维护的项目，修复问题需要一定的时间成本。
+        为了提供更好的服务，
         <br />
-        我很有可能已经发现了BUG所在，并已在着手修复。
+        服务器正在升级。
+        <br />
+        请稍后进行访问。
         <br />
         你也可以通过邮件向我报告问题。
         <br />
