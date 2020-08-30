dad6a0f95bb51720fa2eed346769dd3b5280fe09
Jannchie <jannchie@gmail.com>
2020-08-31 02:39:12 +0900

    Fix: Date

--- src/components/common/UserListItem.vue
+++ src/components/common/UserListItem.vue
@@ -1,29 +1,35 @@
 <template>
   <BiliobCard>
-    <div style="position:absolute; top:-20px" :class="`mx-auto pb-5 title-div`">
+    <div
+      style="position:absolute; top:-20px"
+      :class="`mx-auto pb-5 title-div`"
+    >
       <img
         v-if="title === '观测站的管理者'"
         height="70"
         src="../../../public/img/pendent/观测站的管理者.png"
-      />
+      >
       <img
         v-if="title === '与天同行的观测者'"
         height="70"
         src="../../../public/img/pendent/与天同行的观测者.png"
-      />
+      >
       <img
         v-if="title === '洞悉法度的观想者'"
         height="70"
         src="../../../public/img/pendent/洞悉法度的观想者.png"
-      />
+      >
       <img
         v-if="title === '本心不渝的追寻者'"
         height="70"
         src="../../../public/img/pendent/本心不渝的追寻者.png"
-      />
+      >
     </div>
     <div style="display:flex;flex-direction:column">
-      <div class="name-div" style="flex:99">
+      <div
+        class="name-div"
+        style="flex:99"
+      >
         <span :class="`title  grey--text text--${brightness}-3`">{{
           name
         }}</span>
@@ -32,11 +38,16 @@
             `subheading font-weight-medium grey--text text--${brightness}-1`
           "
         >
-          <span class="mr-1" style="border-radius: 5px">EXP</span> {{ exp }}
+          <span
+            class="mr-1"
+            style="border-radius: 5px"
+          >EXP</span> {{ exp }}
         </div>
       </div>
     </div>
-    <div class="caption grey--text date-div">{{ formatedDate }}</div>
+    <div class="caption grey--text date-div">
+      {{ formatedDate }}
+    </div>
   </BiliobCard>
 </template>
 <script>
