2c2563a7cd79a7b83f36e968b739b9f5768f566d
jannchie <jannchie@gmail.com>
2020-11-12 01:47:16 +0900

    Remove: Useless

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,3 @@
-if (workbox) {
-  console.log(`Yay! Workbox is loaded!`);
-} else {
-  console.log(`Boo! Workbox didn't load!`);
-}
-
 // 设置缓存前缀和后缀，请根据实际项目名修改
 workbox.core.setCacheNameDetails({
   prefix: "biliob",
